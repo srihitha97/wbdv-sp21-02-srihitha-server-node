916d8b3f84851171d551d7702fb85d39871d7027
Srihitha <saisrihithay@gmail.com>
2021-04-20 01:48:18 -0400

    changes

--- server.js
+++ server.js
@@ -33,4 +33,4 @@ require('./controllers/quizzes-controller')(app);
 require('./controllers/question-controller')(app);
 require('./controllers/quiz-attempts-controller')(app);
 
-app.listen(process.env.PORT || 4000);
+app.listen(process.env.PORT || 3001);
